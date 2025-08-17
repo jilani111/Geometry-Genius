c4b2c9a369f2f3b0bcc65c0719817052dfba1eb4
jilani111 <habibullahjilanis@gmail.com>
2025-08-18 01:16:00 +0600

    added triangle box

---
+++ js/triangle.js
@@ -0,0 +1,28 @@
+/*
+* objective: get base, height of a triangle. Calculate the area by using the
+  provided formula. and then display the area.
+* step-1: get the base value of the traingle
+* step-2: added an id in the base input field
+* step-3: use getElementbyId to access the input field
+* step-4: get value from the input field. (value is string now)
+* step-5: convert the value to a number. Use pareFloat()
+*/
+function calculateTriangleArea(){
+    //get base value
+    const baseField = document.getElementById('base_triangle_field');
+    const baseFieldText = baseField.value;
+    const base = parseFloat(baseFieldText);
+    //console.log(base);
+
+    //get height value
+    const heightField = document.getElementById('height_triangle_field');
+    const heightFieldText = heightField.value;
+    const height = parseFloat(heightFieldText);
+    //console.log(height);
+
+    const tringleArea = 0.5 * base * height;
+    console.log(tringleArea);
+    
+    const calculateArea = document.getElementById('claculated_triangle_area');
+    calculateArea.innerText = tringleArea;
+}
